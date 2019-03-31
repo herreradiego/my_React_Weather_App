8925b0f622cf72f3b2b26ef89be9fd7979f0fa35
herreradiego <m.diego.herrera@gmail.com>
2019-03-31 12:42:53 -0300

    City title fixed

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ const Home = (props)=>{
     return(
       <div className="home">
         <h2 className="weather-title">Welcome stranger,
-        weather conditions for {props.userData.suburb}</h2>
+        weather conditions for {props.userData.city}</h2>
         {<WeatherIcon className="weather-Icon" name="owm" iconId={props.weatherData.id.toString()}  flip="horizontal" rotate="90" />}
         <h3 className='weather-description'> {props.weatherData.description}</h3>
         <h4 className='weather-temp'>temp: {Math.round(props.weatherData.temp)}℃</h4>
